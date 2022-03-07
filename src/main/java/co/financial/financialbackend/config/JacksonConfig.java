b9e43f179d908446c2bb393d654aa08dcf5b4a69
Leonardo Freitas <lfreitas.lpf@gmail.com>
2022-03-07 19:31:20 -0300

    Adding JacksonConfig and ModelMapperConfig files

---
+++ src/main/java/co/financial/financialbackend/config/JacksonConfig.java
@@ -0,0 +1,23 @@
+package co.financial.financialbackend.config;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper()
+                .setSerializationInclusion(NON_NULL)
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                .configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true)
+                .configure(DeserializationFeature.UNWRAP_ROOT_VALUE, false)
+                .registerModule(new JavaTimeModule());
+    }
+}
